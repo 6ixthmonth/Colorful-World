56e8dce709f99679fd563667549206469e0aef79
June <6ixthmonth@gmail.com>
2021-10-26 17:46:46 +0900

    Build Main algorithm

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+from random import randrange
 
 from flask import Flask, render_template
 
@@ -30,6 +31,42 @@ def create_app(test_config=None):
 
     @app.route('/play', methods=["GET"])
     def play():
-        return render_template('play.html')
+        target_val_list = [[randrange(0x100) for _ in range(3)] for _ in range(3)]
+        # target_val_list = [[0, 0, 0], [1, 1, 1], [127, 127, 127], [128, 128, 128], [254, 254, 254], [255, 255, 255]] # for check
+
+        print("target")
+        print(target_val_list)
+
+        left_val_list = [[] for _ in target_val_list]
+        right_val_list = [[] for _ in target_val_list]
+        # max_gap_list = [[] for _ in target_val_list] # for check
+        for i in range(len(target_val_list)):
+            for target_rgb in target_val_list[i]:
+                max_gap = target_rgb if target_rgb < 128 else 255 - target_rgb
+                # max_gap_list[i].append(max_gap) # for check
+
+                if (max_gap != 0):
+                    max_gap = randrange(max_gap + 1)
+
+                left_val_list_rgb = target_rgb - max_gap
+                right_val_list_rgb = target_rgb * 2 - left_val_list_rgb
+                left_val_list[i].append(left_val_list_rgb)
+                right_val_list[i].append(right_val_list_rgb)
+
+        # for check
+        # print("max_gap")
+        # print(max_gap_list)
+
+        print("left")
+        print(left_val_list)
+        print("right")
+        print(right_val_list)
+
+        # for check
+        # chk_list = [target_val_list[i][j] * 2 == (left_val_list[i][j] + right_val_list[i][j]) for i in range(len(target_val_list)) for j in range(len(target_val_list[i]))]
+        # print("check")
+        # print(chk_list)
+
+        return render_template('play.html', target=target_val_list, left=left_val_list, right=right_val_list)
 
     return app
