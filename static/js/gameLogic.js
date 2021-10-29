7dbf7b000e2acb480b1dd8542b3de07768d3b6ad
June <6ixthmonth@gmail.com>
2021-10-29 15:46:28 +0900

    Update Rotating algorithm

--- static/js/gameLogic.js
+++ static/js/gameLogic.js
@@ -1,12 +1,14 @@
+const GAME_WINDOW_WIDTH = 800;
+const GAME_WINDOW_HEIGHT = 600;
+
 const GAME_CONFIG = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: GAME_WINDOW_WIDTH,
+    height: GAME_WINDOW_HEIGHT,
     title: "Colorful World - Make the World more Colorful",
     backgroundColor: "#808080",
-    scene_obj: "outer",
+    parent: "wrapper",
     autoCenter: true,
-    // scene: [MAIN_SCENE, GAME_SCENE]
     scene: {
         preload: preload,
         create: create,
@@ -14,147 +16,214 @@ const GAME_CONFIG = {
     }
 };
 
-const game_component = {};
-
-// const MAIN_SCENE = {
-//     key: "main_scene",
-//     create: main_create
-// }
-
-// const GAME_SCENE = {
-//     key: "game_scene",
-//     create: game_create,
-//     update: game_update
-// }
+const GAME_COMPONENT = {};
 
 function preload() {
-    // let target = document.getElementById("target").value;
-    // console.log("target: " + target);
-    // console.log(JSON.parse(target));
-    // let left = document.getElementById("left").value;
-    // console.log("left: " + left);
-    // console.log(JSON.parse(left));
-    // let right = document.getElementById("right").value;
-    // console.log("right: " + right);
-    // console.log(JSON.parse(right));
-}
-
-function create() {
-    console.log(Phaser.Input.Keyboard.KeyCodes);
-    createGrid(this); // for layout
-    createTarget(this);
-    createPalette(this);
-    createLeft(this);
-    createRight(this);
-    createBackground(this);
-
-    createKey(this);
-}
-
-function createGrid(scene_obj) {
-    scene_obj.add.rectangle(400 - 200, 300 - 150, 400, 300, 0x404040); // upper left
-    scene_obj.add.rectangle(400 + 200, 300 + 150, 400, 300, 0x404040); // lower right
-    scene_obj.add.rectangle(400 - 200 - 100, 300 + 150 - 75, 200, 150, 0xc0c0c0); // lower left upper left
-    scene_obj.add.rectangle(400 - 200 + 100, 300 + 150 + 75, 200, 150, 0xc0c0c0); // lower left lower right
-    scene_obj.add.rectangle(400 - 200 - 100 - 50, 300 + 150 + 75 - 37.5, 100, 75, 0x404040); // lower left lower left upper left
-    scene_obj.add.rectangle(400 - 200 - 100 + 50, 300 + 150 + 75 + 37.5, 100, 75, 0x404040); // lower left lower left lower right
-}
-
-function createTarget(scene_obj) {
-    game_component.target_roof = scene_obj.add.triangle(200, 450, 0, 25, 50, -25, 100, 25, 0xff0000);
-    game_component.target_body = scene_obj.add.rectangle(200, 475, 50, 50, 0x00ff00);
-    game_component.target_window = scene_obj.add.rectangle(200, 475, 25, 25, 0x0000ff);
-    scene_obj.add.rectangle(200, 475, 25, 5, 0x000000);
-    scene_obj.add.rectangle(200, 475, 5, 25, 0x000000);
-}
-
-function createPalette(scene_obj) {
-    game_component.palette_roof = scene_obj.add.triangle(400, 300, 0, 50, 100, -50, 200, 50, 0x00ffff);
-    game_component.palette_body = scene_obj.add.rectangle(400, 350, 100, 100, 0xff00ff);
-    game_component.palette_window = scene_obj.add.rectangle(400, 350, 50, 50, 0xffff00);
-    scene_obj.add.rectangle(400, 350, 50, 10, 0x000000);
-    scene_obj.add.rectangle(400, 350, 10, 50, 0x000000);
-}
-
-function createLeft(scene_obj) {
-    // scene_obj.add.circle(0, 300, 100, 0x000000);
-    game_component.left_0 = scene_obj.add.circle(100, 300, 50, 0xff0000);
-    game_component.left_1 = scene_obj.add.circle(50, 300 + Math.sin(Math.PI / 180 * 60) * 100, 50, 0x00ff00);
-    game_component.left_2 = scene_obj.add.circle(-50, 300 + Math.sin(Math.PI / 180 * 60) * 100, 50, 0x0000ff);
-    game_component.left_3 = scene_obj.add.circle(-100, 300, 50, 0x00ffff);
-    game_component.left_4 = scene_obj.add.circle(-50, 300 - Math.sin(Math.PI / 180 * 60) * 100, 50, 0xff00ff);
-    game_component.left_5 = scene_obj.add.circle(50, 300 - Math.sin(Math.PI / 180 * 60) * 100, 50, 0xffff00);
-
-    game_component.left_0.setStrokeStyle(10, 0x000000);
-    game_component.left_1.setStrokeStyle(10, 0x000000);
-    game_component.left_2.setStrokeStyle(10, 0x000000);
-    game_component.left_3.setStrokeStyle(10, 0x000000);
-    game_component.left_4.setStrokeStyle(10, 0x000000);
-    game_component.left_5.setStrokeStyle(10, 0x000000);
-}
-
-function createRight(scene_obj) {
-    // scene_obj.add.circle(800, 300, 100, 0x000000);
-    game_component.right_0 = scene_obj.add.circle(700, 300, 50, 0xff0000);
-    game_component.right_1 = scene_obj.add.circle(750, 300 + Math.sin(Math.PI / 180 * 60) * 100, 50, 0x00ff00);
-    game_component.right_2 = scene_obj.add.circle(850, 300 + Math.sin(Math.PI / 180 * 60) * 100, 50, 0x0000ff);
-    game_component.right_3 = scene_obj.add.circle(900, 300, 50, 0x00ffff);
-    game_component.right_4 = scene_obj.add.circle(850, 300 - Math.sin(Math.PI / 180 * 60) * 100, 50, 0xff00ff);
-    game_component.right_5 = scene_obj.add.circle(750, 300 - Math.sin(Math.PI / 180 * 60) * 100, 50, 0xffff00);
-
-    game_component.right_0.setStrokeStyle(10, 0x000000);
-    game_component.right_1.setStrokeStyle(10, 0x000000);
-    game_component.right_2.setStrokeStyle(10, 0x000000);
-    game_component.right_3.setStrokeStyle(10, 0x000000);
-    game_component.right_4.setStrokeStyle(10, 0x000000);
-    game_component.right_5.setStrokeStyle(10, 0x000000);
-}
-
-function createBackground(scene_obj) {
     
 }
 
-function createKey(scene_obj) {
-    // game_component.keyUp = scene_obj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-    // game_component.keyDown = scene_obj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-    // game_component.keyLeft = scene_obj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    // game_component.keyRight = scene_obj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    // game_component.keySpace = scene_obj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+function create() {
+    createGrid(this); // for layout
 
-    game_component.cursors = scene_obj.input.keyboard.createCursorKeys();
+    createTarget(this);
+    createPalette(this);
+    createCursor(this);
+    createLeftController(this);
+    createRightController(this);
+    createBackground(this);
+
+    createKey(this);
+
+    console.log(GAME_COMPONENT);
 }
 
+function createGrid(sceneObj) {
+    sceneObj.add.rectangle(400 - 200, 300 - 150, 400, 300, 0x404040); // upper left
+    sceneObj.add.rectangle(400 + 200, 300 + 150, 400, 300, 0x404040); // lower right
+    sceneObj.add.rectangle(400 - 200 - 100, 300 + 150 - 75, 200, 150, 0xc0c0c0); // lower left upper left
+    sceneObj.add.rectangle(400 - 200 + 100, 300 + 150 + 75, 200, 150, 0xc0c0c0); // lower left lower right
+    sceneObj.add.rectangle(400 - 200 - 100 - 50, 300 + 150 + 75 - 37.5, 100, 75, 0x404040); // lower left lower left upper left
+    sceneObj.add.rectangle(400 - 200 - 100 + 50, 300 + 150 + 75 + 37.5, 100, 75, 0x404040); // lower left lower left lower right
+}
+
+function createTarget(sceneObj) {
+    GAME_COMPONENT.target = []
+    GAME_COMPONENT.target[0] = sceneObj.add.triangle(200, 450, 0, 25, 50, -25, 100, 25, 0x808080, 0.5).setStrokeStyle(1, 0x000000);
+    GAME_COMPONENT.target[1] = sceneObj.add.rectangle(200, 475, 50, 50, 0x808080, 0.5).setStrokeStyle(1, 0x000000);
+    GAME_COMPONENT.target[2] = sceneObj.add.rectangle(200, 475, 25, 25, 0x808080, 0.5).setStrokeStyle(1, 0x000000);
+    sceneObj.add.rectangle(200, 475, 25, 5, 0x000000);
+    sceneObj.add.rectangle(200, 475, 5, 25, 0x000000);
+
+    let target = JSON.parse(document.getElementById("target").value);
+    for (let i = 0; i < target.length; i++) {
+        let c = Phaser.Display.Color.GetColor(target[i][0], target[i][1], target[i][2]);
+        GAME_COMPONENT.target[i].setFillStyle(c, 1);
+    }
+}
+
+function createPalette(sceneObj) {
+    GAME_COMPONENT.palette = []
+    GAME_COMPONENT.palette[0] = sceneObj.add.triangle(400, 300, 0, 50, 100, -50, 200, 50, 0xffffff, 1.0).setStrokeStyle(1, 0x000000);
+    GAME_COMPONENT.palette[1] = sceneObj.add.rectangle(400, 350, 100, 100, 0xffffff, 1.0).setStrokeStyle(1, 0x000000);
+    GAME_COMPONENT.palette[2] = sceneObj.add.rectangle(400, 350, 50, 50, 0xffffff, 1.0).setStrokeStyle(1, 0x000000);
+    sceneObj.add.rectangle(400, 350, 50, 10, 0x000000);
+    sceneObj.add.rectangle(400, 350, 10, 50, 0x000000);
+}
+
+function createCursor(sceneObj) {
+    GAME_COMPONENT.cursor = []
+    GAME_COMPONENT.cursor[0] = sceneObj.add.triangle(400, 300, 0, 50, 100, -50, 200, 50, 0xffff00, 0.5);
+    GAME_COMPONENT.cursor[1] = sceneObj.add.rectangle(400, 350, 100, 100, 0xffff00, 0.5);
+    GAME_COMPONENT.cursor[2] = sceneObj.add.rectangle(400, 350, 50, 50, 0xffff00, 0.5);
+
+    sceneObj.tweens.add({
+        targets: GAME_COMPONENT.cursor[0],
+        alpha: 0.5,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut'
+    });
+
+    sceneObj.tweens.add({
+        targets: GAME_COMPONENT.cursor[1],
+        alpha: 0.5,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut'
+    });
+
+    sceneObj.tweens.add({
+        targets: GAME_COMPONENT.cursor[2],
+        alpha: 0.5,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut'
+    });
+
+    GAME_COMPONENT.cursor[0].visible = true;
+    GAME_COMPONENT.cursor[1].visible = false;
+    GAME_COMPONENT.cursor[2].visible = false;
+}
+
+function createLeftController(sceneObj) {
+    // sceneObj.add.circle(0, 300, 100, 0x000000);
+    // start from right, locate clockwisely
+    let circleRadius = 50;
+    let signDistance = Math.sin(Math.PI / 180 * 60) * circleRadius * 2;
+    GAME_COMPONENT.leftController = []
+    GAME_COMPONENT.leftController[0] = sceneObj.add.circle(0 + 100, 300 + 0, circleRadius, 0xff0000).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.leftController[1] = sceneObj.add.circle(0 + 50, 300 + signDistance, circleRadius, 0x00ff00).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.leftController[2] = sceneObj.add.circle(0 -50, 300 + signDistance, circleRadius, 0x0000ff).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.leftController[3] = sceneObj.add.circle(0 -100, 300 - 0, circleRadius, 0x00ffff).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.leftController[4] = sceneObj.add.circle(0 -50, 300 - signDistance, circleRadius, 0xff00ff).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.leftController[5] = sceneObj.add.circle(0 + 50, 300 - signDistance, circleRadius, 0xffff00).setStrokeStyle(10, 0x000000);
+
+    let left = JSON.parse(document.getElementById("left").value);
+    for (let i = 0; i < left.length; i++) {
+        let c = Phaser.Display.Color.GetColor(left[i][0], left[i][1], left[i][2]);
+        GAME_COMPONENT.leftController[i].setFillStyle(c, 1);
+    }
+}
+
+function createRightController(sceneObj) {
+    // sceneObj.add.circle(800, 300, 100, 0x000000);
+    // start from left, locate counter-clockwisely
+    let circleRadius = 50;
+    let signDistance = Math.sin(Math.PI / 180 * 60) * circleRadius * 2;
+    GAME_COMPONENT.rightController = []
+    GAME_COMPONENT.rightController[0] = sceneObj.add.circle(GAME_WINDOW_WIDTH - 100, 300 + 0, circleRadius, 0xff0000).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.rightController[1] = sceneObj.add.circle(GAME_WINDOW_WIDTH - 50, 300 + signDistance, circleRadius, 0x00ff00).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.rightController[2] = sceneObj.add.circle(GAME_WINDOW_WIDTH + 50, 300 + signDistance, circleRadius, 0x0000ff).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.rightController[3] = sceneObj.add.circle(GAME_WINDOW_WIDTH + 100, 300 - 0, circleRadius, 0x00ffff).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.rightController[4] = sceneObj.add.circle(GAME_WINDOW_WIDTH + 50, 300 - signDistance, circleRadius, 0xff00ff).setStrokeStyle(10, 0x000000);
+    GAME_COMPONENT.rightController[5] = sceneObj.add.circle(GAME_WINDOW_WIDTH - 50, 300 - signDistance, circleRadius, 0xffff00).setStrokeStyle(10, 0x000000);
+
+    let right = JSON.parse(document.getElementById("right").value);
+    for (let i = 0; i < GAME_COMPONENT.rightController.length; i++) {
+        let c = Phaser.Display.Color.GetColor(right[i][0], right[i][1], right[i][2]);
+        GAME_COMPONENT.rightController[i].setFillStyle(c, 1);
+    }
+}
+
+function createBackground(sceneObj) {
+    
+}
+
+function createKey(sceneObj) {
+    // GAME_COMPONENT.keyUp = sceneObj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    // GAME_COMPONENT.keyDown = sceneObj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    // GAME_COMPONENT.keyLeft = sceneObj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    // GAME_COMPONENT.keyRight = sceneObj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    // GAME_COMPONENT.keySpace = sceneObj.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    GAME_COMPONENT.cursors = sceneObj.input.keyboard.createCursorKeys();
+}
+
+let isLeftRotating = false;
+let leftRotateDeg = 0;
+let isRightRotating = false;
+let rightRotateDeg = 0;
+const ONE_DEG = Math.PI / 180 * 1;
+
+let left_idx = 0;
+let right_idx = 0;
+let cursor_idx = 0;
+
 function update() {
-    // if (game_component.keyUp.isDown) {
+    // if (GAME_COMPONENT.keyUp.isDown) {
     //     console.log("UP");
     // }
-    // if (game_component.keyDown.isDown) {
+    // if (GAME_COMPONENT.keyDown.isDown) {
     //     console.log("DOWN");
     // }
-    // if (game_component.keyLeft.isDown) {
+    // if (GAME_COMPONENT.keyLeft.isDown) {
     //     console.log("LEFT");
     // }
-    // if (game_component.keyRight.isDown) {
+    // if (GAME_COMPONENT.keyRight.isDown) {
     //     console.log("RIGHT");
     // }
-    // if (game_component.keySpace.isDown) {
+    // if (GAME_COMPONENT.keySpace.isDown) {
     //     console.log("SPACE");
     // }
 
-    if (this.input.keyboard.checkDown(game_component.cursors.left, 1000)) {
-        console.log("LEFT");
-    } else if (this.input.keyboard.checkDown(game_component.cursors.right, 1000)) {
-        console.log("RIGHT");
+    if (this.input.keyboard.checkDown(GAME_COMPONENT.cursors.left, 1000)) {
+        isLeftRotating = true;
+    } else if (this.input.keyboard.checkDown(GAME_COMPONENT.cursors.right, 1000)) {
+        isRightRotating = true;
     }
 
-    if (this.input.keyboard.checkDown(game_component.cursors.up, 1000)) {
-        console.log("UP");
-    } else if (this.input.keyboard.checkDown(game_component.cursors.down, 1000)) {
-        console.log("DOWN");
+    if (this.input.keyboard.checkDown(GAME_COMPONENT.cursors.up, 1000)) {
+        GAME_COMPONENT.cursor[(((cursor_idx % 3) + 3) % 3)].visible = false;
+        cursor_idx--;
+        GAME_COMPONENT.cursor[(((cursor_idx % 3) + 3) % 3)].visible = true;
+    } else if (this.input.keyboard.checkDown(GAME_COMPONENT.cursors.down, 1000)) {
+        GAME_COMPONENT.cursor[(((cursor_idx % 3) + 3) % 3)].visible = false;
+        cursor_idx++;
+        GAME_COMPONENT.cursor[(((cursor_idx % 3) + 3) % 3)].visible = true;
     }
 
-    if (this.input.keyboard.checkDown(game_component.cursors.space, 1000)) {
+    if (this.input.keyboard.checkDown(GAME_COMPONENT.cursors.space, 1000)) {
         console.log("SPACE");
+    }
+
+    if (isLeftRotating) {
+        Phaser.Actions.RotateAround(GAME_COMPONENT.leftController, { x: 0, y: 300 }, ONE_DEG * -1);
+        leftRotateDeg++;
+        if (leftRotateDeg % 60 == 0) {
+            left_idx++;
+            console.log("left_idx: " + (left_idx % 6));
+            isLeftRotating = false;
+        }
+    }
+
+    if (isRightRotating) {
+        Phaser.Actions.RotateAround(GAME_COMPONENT.rightController, { x: 800, y: 300 }, ONE_DEG * 1);
+        rightRotateDeg++;
+        if (rightRotateDeg % 60 == 0) {
+            right_idx++
+            console.log("right_idx: " + (right_idx % 6));
+            isRightRotating = false;
+        }
     }
 }
 
